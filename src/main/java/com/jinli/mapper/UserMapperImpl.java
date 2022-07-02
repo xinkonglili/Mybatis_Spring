63b461cab89557f15d0d6b5c3ede890f8c3f9e27
wllwo <wllworkemail@163.com>
2022-07-02 22:44:48 +0800

    Initial commit

---
+++ src/main/java/com/jinli/mapper/UserMapperImpl.java
@@ -0,0 +1,22 @@
+package com.jinli.mapper;
+
+import com.jinli.pojo.User;
+import org.mybatis.spring.SqlSessionTemplate;
+import java.util.List;
+
+public class UserMapperImpl implements UserMapper{
+
+    //原来我们所有的操作都是使用sqlSession来执行的，现在都是用SqlSessionTemplate
+
+    private SqlSessionTemplate sqlSession;
+    public void setSqlSession(SqlSessionTemplate sqlSession) {
+        this.sqlSession = sqlSession;
+    }
+
+    @Override
+    public List<User> selectUser() {
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        return mapper.selectUser();
+
+    }
+}
---
+++ src/test/java/MyTest.java
@@ -0,0 +1,38 @@
+import com.jinli.mapper.UserMapper;
+import com.jinli.pojo.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+//spring整合mybatis之后，就不用在测试类里面去getMapper了，调用实现接口的那个类里面的方法就好了
+
+public class MyTest {
+    @Test
+    public void test() throws IOException {
+       /* //整合了spring之后，再也不用这些东西了
+        String resource = "mybatis-config.xml";
+        InputStream in = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(in);
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        List<User> userList = mapper.selectUser();
+
+        for (User user : userList) {
+            System.out.println(user);
+        }*/
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-dao.xml");
+        UserMapper userMapper = context.getBean("UserMapper",UserMapper.class);
+        for (User user : userMapper.selectUser()) {
+            System.out.println(user);
+        }
+
+    }
+}
